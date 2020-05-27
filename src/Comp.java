a1f675ecdf5d77317ad0d0b1068906ded5bd6492
liudayun <22281334@qq.com>
2020-05-27 22:59:36 +1000

    update for read file

--- src/Comp.java
+++ src/Comp.java
@@ -2,5 +2,7 @@ public class Comp {
     public static void main(String[] args) {
         Read_File rf=new Read_File();
         rf.readPhonebook("contacts_2.txt");
+        rf.readPhonebook("instructions_3.txt");
+
     }
 }
