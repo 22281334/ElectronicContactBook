8401c34322db2a80b7d71a898d8c183a89c4d41b
liudayun <22281334@qq.com>
2020-05-31 19:36:43 +1000

    finish

--- src/Comp.java
+++ src/Comp.java
@@ -1,8 +1,11 @@
 public class Comp {
     public static void main(String[] args) {
         Phone_Book main=new Phone_Book();
-        main.readPhonebook(args[0]);
-        main.readInstruction(args[1],args[2]);
-        main.printQueryReport(args[3]);
+        main.readPhonebook("contacts_1.txt");
+        main.readInstruction("instructions_1.txt","out.txt");
+        main.printQueryReport("report.txt");
+//        main.readPhonebook(args[0]);
+//        main.readInstruction(args[1],args[2]);
+//        main.printQueryReport(args[3]);
     }
 }
